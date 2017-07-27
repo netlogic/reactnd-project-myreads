a6f2f6feee1f56fc1236bc37d98c295ac16b2c6f
Bret <Bret@Netlogic.com>
2017-07-26 22:12:59 -0400

    search api not working

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import '../App.css'
+import * as BooksAPI from '../BooksAPI'
+import sortBy from 'sort-by'
+
 
 export default class BooksApp extends React.Component {
 
+    state = {
+        query: '',
+        books: [],
+    }
+
+    updateQuery = (query) => {
+        let q = query.trim();
+        if (q) {
+            this.setState({ query: query.trim() })
+            this.executeQuery();
+        } else {
+            this.clearQuery();
+        }
+    }
+
+    clearQuery = () => {
+        this.setState({ query: '', books: [] })
+    }
+
+    componentDidMount() {
+        this.executeQuery();
+    }
+
+    executeQuery() {
+        BooksAPI.search(this.state.query, 10).then(books => {
+            this.setState({ books: books.sort(sortBy('title')) });
+        }).catch(function () {
+            alert("An error happen talking to the book server.  Please check your internet connection and refresh page to try again!")
+        });
+    }
+
     render() {
+        let { query } = this.state;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -17,12 +53,17 @@ export default class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text" placeholder="Search by title or author"
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+
+                    </ol>
                 </div>
             </div>
         )
